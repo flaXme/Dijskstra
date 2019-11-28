f51cbb7001dca0d19fd0ae4f799381482c12abe5
st159513 <st159613@stud.uni-stuttgart.de P>
2019-11-28 20:58:36 +0100

    empty maven project created

---
+++ programmierprojekt/src/main/java/backend/phase/one/programmierprojekt/App.java
@@ -0,0 +1,13 @@
+package backend.phase.one.programmierprojekt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
