dbbe4f87fa06d9c63e70544b63fbb5f2be2f1659
st159513 <st159613@stud.uni-stuttgart.de P>
2019-11-29 09:50:47 +0100

    Dikstra classes erstellt

--- programmierprojekt/src/main/java/backend/phase/one/programmierprojekt/Dijkstra_0.java
+++ programmierprojekt/src/main/java/backend/phase/one/programmierprojekt/Dijkstra_0.java
@@ -1,5 +1,9 @@
 package backend.phase.one.programmierprojekt;
-
+/**
+ * 
+ * @author Ahmed Ebrahim Aldekal
+ *
+ */
 public class Dijkstra_0 {
-	
+	Graph g = new Graph("path hier");
 }
--- programmierprojekt/src/main/java/backend/phase/one/programmierprojekt/Dijkstra_1.java
+++ programmierprojekt/src/main/java/backend/phase/one/programmierprojekt/Dijkstra_1.java
@@ -1,5 +1,8 @@
 package backend.phase.one.programmierprojekt;
-
+/**
+ * 
+ * @author XinPang
+ */
 public class Dijkstra_1 {
-
+	Graph g = new Graph("path hier");
 }
