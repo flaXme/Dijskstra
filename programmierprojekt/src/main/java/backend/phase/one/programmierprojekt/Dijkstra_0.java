3b189822cb852bbaee1c92d44a37c92a75df433c
st159513 <st159613@stud.uni-stuttgart.de P>
2019-11-29 12:32:08 +0100

    dijkstra erst versuche

--- programmierprojekt/src/main/java/backend/phase/one/programmierprojekt/Dijkstra_0.java
+++ programmierprojekt/src/main/java/backend/phase/one/programmierprojekt/Dijkstra_0.java
@@ -5,5 +5,33 @@ package backend.phase.one.programmierprojekt;
  *
  */
 public class Dijkstra_0 {
-	Graph g = new Graph("path hier");
+	Graph graph;
+	int startNodeID;
+	int[] costFromStartNodeToAllOtherNodes;
+	
+	public Dijkstra_0(Graph graph, int startNodeID) {
+		this.graph = graph;
+		this.startNodeID = startNodeID;
+		costFromStartNodeToAllOtherNodes = dijkstraOneToAll(graph, startNodeID);
+	}
+	
+	private int[] dijkstraOneToAll(Graph graph, int startNodeID) {
+		System.out.println("calculate shortest path");
+		int [] costToOther = new int[graph.getNodeNr()];
+		for (int i = 0; i < costToOther.length; i++) {// for all nodes make the shortest path cost -1
+			costToOther[i] = -1;
+		}
+		costToOther[startNodeID] = 0; // shortest path from startNode to itself is zero
+		
+		
+		return costToOther;
+	}
+	
+	private int[] getOutgoingEdgeWithCost(int nodeID) {
+		int[] outgoingEdges = new int[graph.getNrOfOutgoingEdges(nodeID)*2];
+		
+		
+		
+		return outgoingEdges;
+	}
 }
--- programmierprojekt/src/main/java/backend/phase/one/programmierprojekt/Main.java
+++ programmierprojekt/src/main/java/backend/phase/one/programmierprojekt/Main.java
@@ -5,11 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		
 		long startTime = System.currentTimeMillis();
-		Graph g = new Graph("/home/ad/Downloads/graph-files/germany.fmi");
+		Graph g = new Graph("/home/ad/Downloads/graph-files/toy.fmi");
 		long endTime = System.currentTimeMillis();
 		long totalTime = endTime-startTime;
 		System.out.println("Time: "+ totalTime/1000 +" s");
 		
+		g.printEdgeArray(g.getEdgeNr()*3);
+		g.printNodeArray(g.getNodeNr());
 	}
 
 }
